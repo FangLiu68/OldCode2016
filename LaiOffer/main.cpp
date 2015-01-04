90c3495e220bb3694144f2b17f007518fc8074b4
Rippler <lfang68@gmail.com>
2015-01-03 19:23:35 -0800

    Initial Commit

---
+++ LaiOffer/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LaiOffer
+//
+//  Created by Fang Liu on 1/3/15.
+//  Copyright (c) 2015 Fang Liu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
